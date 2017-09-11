af3935c991b9b69e3277dd8831e672814c7fed20
atul-srivastava <30436617+atul-srivastava@users.noreply.github.com>
2017-09-11 16:54:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,28 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+> make.power <- function(n) {
++ pow <- function(x) {
++ x^n
++ }
++ pow
++ }
+> cube <- make.power(3)
+> square <- make.power(2)
+> cube(3)
+[1] 27
+> square(3)
+[1] 9
+> cube
+function(x) {
+x^n
+}
+<environment: 0x7ff460527478>
+> ls(environment(cube))
+[1] "n" "pow"
+> get("n", environment(cube))
+[1] 3
+> ls(environment(square))
+[1] "n" "pow"
+> get("n", environment(square))
+[1] 2
